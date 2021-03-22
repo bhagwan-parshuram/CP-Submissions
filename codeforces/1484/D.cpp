c1f43b2bfe6bd010aedbd83c663901d5acf2eb97
Avinash Kumar Singh <avinash007kumarsingh@gmail.com>
2021-03-23 01:30:00 +0530

    Add solution for problem `D - Playlist` Link: https://codeforces.com/contest/1484/problem/D Tags: data structures, dsu, implementation, *1900 Ref: https://codeforces.com/contest/1484/submission/110767921

---
+++ codeforces/1484/D.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100005
+#define endl "\n"
+
+//linked list type implementation
+int nxt[MAX];
+int visited[MAX];
+int a[MAX];
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        queue<int> q;
+        for (int i = 0; i < n;i++){
+            cin >> a[i];
+            nxt[i] = (i + 1)%n;
+            visited[i] = 0;
+        }
+        for (int i = 0; i < n;i++){
+            if(__gcd(a[i],a[nxt[i]])==1){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            if(visited[curr])
+                continue;
+            if(!visited[nxt[curr]]){
+                ans.push_back(nxt[curr]);
+                visited[nxt[curr]] = 1;
+            }
+            nxt[curr] = nxt[nxt[curr]];
+            if(__gcd(a[curr],a[nxt[curr]])==1)
+                    q.push(curr);
+        }
+        cout << ans.size() << " ";
+        for (int i = 0; i < ans.size();i++)
+            cout << ans[i] + 1 << " ";
+        cout << endl;
+    }
+}
