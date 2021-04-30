930e1f8927170805eb7928cc7dfc84202812e81c
Avinash Kumar Singh <avinash007kumarsingh@gmail.com>
2021-04-30 14:16:00 +0530

    Add solution for problem `D - Colorful Graph` Link: https://codeforces.com/contest/246/problem/D Tags: brute force, dfs and similar, graphs, *1600 Ref: https://codeforces.com/contest/246/submission/114669054

---
+++ codeforces/246/D.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+typedef vector<vll> vvl;
+typedef vector<pll> vpll; 
+ 
+#define endl '\n' 
+#define fast std::ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+
+ll n,m;
+vvi g;
+set<int> s[100005];
+bool visited[100005];
+int color[100005];
+void dfs(int node){
+	visited[node]=1;
+	for(auto u: g[node]){
+		if(color[u]!=color[node])s[color[node]].insert(color[u]);
+		if(!visited[u])dfs(u);
+		// s[color[u]].insert(color[node]);
+	}
+}
+void solve(){
+	cin>>n>>m;
+	g.clear();
+	g.resize(n+1);
+	for(int i=1;i<=n;i++){
+		cin>>color[i]; 
+	}
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin>>a>>b;
+		g[a].pb(b);
+	    g[b].pb(a);
+	}
+	
+	
+	for(int i=1;i<=n;i++){
+		if(!visited[i])dfs(i);
+	}
+	int c1=-1;
+	int maxi=-1;
+	for(int i=1;i<=n;i++){
+		if(int(s[color[i]].size())>maxi){
+			maxi=s[color[i]].size();
+			c1=color[i];
+		}
+		else if(int(s[color[i]].size())==maxi){
+			if(c1>color[i])c1=color[i];
+		}
+	}
+	cout<<c1<<endl;
+
+}
+
+int main(){
+	fast;
+	int t=1;
+	while(t--)
+	  solve();
+}
