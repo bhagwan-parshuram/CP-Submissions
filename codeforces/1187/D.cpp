9a2a91db33a562f60c857ee35c6cdcfc4fc61bf8
Avinash Kumar Singh <avinash007kumarsingh@gmail.com>
2021-05-05 18:46:00 +0530

    Add solution for problem `D - Subarray Sorting` Link: https://codeforces.com/contest/1187/problem/D Tags: data structures, sortings, *2400 Ref: https://codeforces.com/contest/1187/submission/115206414

--- codeforces/1187/D.cpp
+++ codeforces/1187/D.cpp
@@ -1,102 +1,80 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-int inf=1e7;
-#define MAX 300005
 
-struct node{
-	int val;
-	node(){
-		val=inf;
-	}
-	void assign(int x){
-		val=x;
-	}
+const int N = int(3e5) + 99;
+const int INF = int(1e9) + 99;
 
-};
-node tree[4*MAX];
-vector<int> st[MAX];
-node merge(node a,node b){
-    node res;
-    res.val=min(a.val,b.val);
-    return res;
+int t, n;
+int a[N], b[N];
+vector <int> p[N];
+int st[4 * N + 55];
+
+int getMin(int v, int l, int r, int L, int R){
+	if(L >= R) return INF;
+	if(l == L && r == R)
+		return st[v];
+
+	int mid = (l + r) / 2;
+	return min(getMin(v * 2 + 1, l, mid, L, min(R, mid)),
+		getMin(v * 2 + 2, mid, r, max(mid, L), R));	
 }
 
-void update(int i,int l,int r,int pos,int x){
-     if(pos <l || pos>r)return;//can reduce complexity
-     if(l==r){
-     	tree[i].assign(x);
-     	return;
-     }
+void upd(int v, int l, int r, int pos, int x){
+	if(r - l == 1){
+		st[v] = x;
+		return;
+	}
 
-     int  mid=(l+r)/2;
-     update(2*i,l,mid,pos,x);
-     update(2*i+1,mid+1,r,pos,x);
-     tree[i]=merge(tree[2*i],tree[2*i+1]);
+	int mid = (l + r) / 2;
+	if(pos < mid) upd(v * 2 + 1, l, mid, pos, x);
+	else upd(v * 2 + 2, mid, r, pos, x);
+		
+	st[v] = min(st[v * 2 + 1], st[v * 2 + 2]);
 }
 
-node query(int i,int l,int r,int lq,int rq){
-	if(lq>r || rq<l){
-		return node();
-	}
-	if(lq<=l && r<=rq){
-		return tree[i];
-	}
-	int mid=(l+r)/2;
-	return merge(query(2*i,l,mid,lq,rq),query(2*i+1,mid+1,r,lq,rq));
-}
-int n;
-int a[MAX];
-int b[MAX];
-void solve(){
-	cin>>n;
-	for(int i=0;i<=n;i++){
-		st[i].clear();
-		st[i].resize(0);
-	}
-	for(int i=0;i<=4*n;i++){
-		tree[i].val=inf;
-	}
-
-	for(int i=0;i<n;i++){
-	  cin>>a[i];
-	  a[i]--;
-	  st[a[i]].push_back(i);
-	}
-	for(int i=0;i<n;i++){
-		cin>>b[i];
-		b[i]--;
-	}
-	for(int i=0;i<n;i++){
-		reverse(st[i].begin(),st[i].end());
-		if(!st[i].empty()){
-	  	update(1,0,n-1,i,st[i].back());
-	  }
-	}
-	for(int i=0;i<n;i++){
-		int curr=b[i];
-		if(st[curr].empty()){
-			cout<<"NO"<<endl;
-			return;
+int main() {
+	scanf("%d", &t);
+	for(int tc = 0; tc < t; ++tc){
+		scanf("%d", &n);
+		for(int i = 0; i < n; ++i)
+			p[i].clear();					
+		for(int i = 0; i < n; ++i){
+			scanf("%d", a + i);
+			--a[i];
+			p[a[i]].push_back(i);	
+		}	
+		for(int i = 0; i < n; ++i){
+			scanf("%d", b + i);	
+			--b[i];		
 		}
-		int pos=st[curr].back();
-		node mini=query(1,0,n-1,0,curr);
-		if(mini.val<pos){
-            cout<<"NO"<<endl;
-			return;
-		}
-		st[curr].pop_back();
-		if(st[curr].empty()){
-			update(1,0,n-1,curr,inf);
-		}
-		else update(1,0,n-1,curr,(st[curr].back()));
-	}
-    cout<<"YES"<<endl;
-}
 
-int main(){
- int t;
- cin>>t;
- while(t--){
- 	solve();
- }
+		for(int i = 0; i < 4 * n; ++i) st[i] = INF;
+		for(int i = 0; i < n; ++i){
+			reverse(p[i].begin(), p[i].end());
+			if(!p[i].empty()) upd(0, 0, n, i, p[i].back());
+		}
+
+		bool ok = true;
+		for(int i = 0; i < n; ++i){
+			if(p[b[i]].empty()){
+				ok = false;
+				break;
+			}
+
+			int pos = p[b[i]].back();
+			if(getMin(0, 0, n, 0, b[i]) < pos){
+				ok = false;
+				break;
+			}
+
+			p[b[i]].pop_back();
+			upd(0, 0, n, b[i], p[b[i]].empty()? INF : p[b[i]].back());			
+		}
+
+		if(ok) puts("YES");
+		else puts("NO");
+	}	
+	
+	return 0;
 }
