9f4b4e397dcf78bf8f7bed114508fcc8134fd4f1
Avinash Kumar Singh <avinash007kumarsingh@gmail.com>
2021-05-11 02:34:00 +0530

    Add solution for problem `D - Distance in Tree` Link: https://codeforces.com/contest/161/problem/D Tags: dfs and similar, dp, trees, *1800 Ref: https://codeforces.com/contest/161/submission/115897397

--- codeforces/161/D.cpp
+++ codeforces/161/D.cpp
@@ -19,32 +19,41 @@ typedef vector<pll> vpll;
 
 ll n,K;
 vvi g;
-ll dp[100005][505];
-ll ans;
-void dfs(ll nn,ll pp){
-    dp[nn][0]=1;
+ll indp[50005][505];
+ll outdp[50005][505];
+void dfs1(ll nn,ll pp){
+    indp[nn][0]=1;
     for(auto u:g[nn]){
         if(u==pp)continue;
-
-        dfs(u,nn);
-        for(int i=0;i<K;i++)ans+=dp[nn][i]*dp[u][K-i-1];
-        //ans+=dp[nn][K];
-        for(int i=0;i<K;i++)dp[nn][i+1]+=dp[u][i];
+        dfs1(u,nn);
+        for(int i=1;i<=K;i++){
+            indp[nn][i]+=indp[u][i-1];
+        } 
     }
 }
-
+void dfs2(int nn,int pp){
+     if(pp!=0){
+      for(int i=1;i<=K;i++)outdp[nn][i]+=outdp[pp][i-1]+indp[pp][i-1]-((i>=2)?indp[nn][i-2]:0);
+     }
+   for(auto u:g[nn]){
+    if(u==pp)continue;
+     dfs2(u,nn);
+   }
+}
 void solve(){
     cin>>n>>K;
     g.resize(n+1);
-    ans=0;
     for(int i=0;i<n-1;i++){
         ll a,b;
         cin>>a>>b;
         g[a].pb(b);
         g[b].pb(a);
     }
-    dfs(1,0);
-    cout<<ans<<endl;
+    ll ans=0;
+    dfs1(1,0);
+    dfs2(1,0);
+    for(int i=1;i<=n;i++)ans+=(indp[i][K]+(outdp[i][K]));
+    cout<<ans/2<<endl;
 }
 
 int main(){
